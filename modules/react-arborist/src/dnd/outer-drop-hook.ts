168b42f12df3122f65780800c6e4561d5b080269
luoyijian <827388178@qq.com>
2025-07-10 16:37:48 +0800

    fix: drop below bottom item trouble

--- modules/react-arborist/src/dnd/outer-drop-hook.ts
+++ modules/react-arborist/src/dnd/outer-drop-hook.ts
@@ -4,6 +4,7 @@ import { DragItem } from "../types/dnd";
 import { computeDrop } from "./compute-drop";
 import { DropResult } from "./drop-hook";
 import { actions as dnd } from "../state/dnd-slice";
+import { ROOT_ID } from "../data/create-root";
 
 export function useOuterDrop() {
   const tree = useTreeApi();
@@ -35,6 +36,18 @@ export function useOuterDrop() {
         } else {
           tree.hideCursor();
         }
+      },
+      drop: (_, monitor) => {
+        if (!monitor.canDrop()) return null;
+        const { parentId, index } = tree.state.dnd;
+        tree.props.onMove?.({
+          dragIds: tree.state.dnd.dragIds,
+          parentId: parentId === ROOT_ID ? null : parentId,
+          index: index === null ? 0 : index,
+          dragNodes: tree.dragNodes,
+          parentNode: tree.get(parentId),
+        });
+        return { parentId, index };
       },
     }),
     [tree]
